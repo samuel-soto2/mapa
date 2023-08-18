fa8b6105bae5e8b51acf5567e4ea0b98429a720f
samuel-soto2 <1461888@senati.pe>
2023-08-17 19:37:09 -0500

    les voy a explicar a continuacion, primero declaramos el metodo principal que seria main,luego declaramos un arreglo de numeros que en este casoseria 5 numeros.

---
+++ ordenar/src/main/java/com/mycompany/ordenar/Ordenar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ordenar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ordenar {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+    }
+}
